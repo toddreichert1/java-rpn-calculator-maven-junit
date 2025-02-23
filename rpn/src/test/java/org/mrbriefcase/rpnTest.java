50b05e32944355fc66161b17676930a59212c469
Todd Reichert <toddreichert@mac.com>
2025-02-23 11:39:25 -0600

    Added cosine test

--- rpn/src/test/java/org/mrbriefcase/rpnTest.java
+++ rpn/src/test/java/org/mrbriefcase/rpnTest.java
@@ -30,4 +30,11 @@ public class rpnTest {
         String[] args = {"12","4","/"};
         assertEquals("3", rpn.main(args));
     }
+    
+    @Test
+    public void cosineTest() {
+        rpn rpn = new rpn();
+        String[] args = {"0","cos"};
+        assertEquals("1", rpn.main(args));
+    }
 }
